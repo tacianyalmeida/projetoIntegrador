132c51cac80dadac142246227099d4abfa98ddb9
Taciany Maria S. <138811627+tacianyalmeida@users.noreply.github.com>
2024-11-25 09:48:58 -0300

    Update Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { BoxFooter, TextosFooter } from "../Styles/Footer.js"
+
+const Footer = () => {
+    return(
+        <BoxFooter>
+            <TextosFooter>©2024 TimePlanner, Inc.</TextosFooter>
+            <TextosFooter>Privacy</TextosFooter>
+            <TextosFooter>Manage Cookies</TextosFooter>
+            <TextosFooter>Do not share my personal information</TextosFooter>
+        </BoxFooter>
+    )
+}
+
+export default Footer;
